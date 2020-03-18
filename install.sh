947efdba19c2a7123e4f4e272f067e9a621502d8
IOTGEMINI <47248395+iotgemini@users.noreply.github.com>
2020-03-18 23:05:26 +0100

    1.6.2.1

--- install.sh
+++ install.sh
@@ -219,6 +219,7 @@ fi
 		echo "Install Node-Red:"
 		bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
 		sudo systemctl enable nodered.service
+		cd ~
 		npm install node-red-contrib-iotgemini
 		npm i node-red-dashboard
 	fi
